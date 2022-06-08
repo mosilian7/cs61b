18bdc262b17a8660ed9fa95a05ffc68a32111363
mosilian7 <3032397952@qq.com>
2022-06-08 15:11:18 +0800

    lab12: bug fix for size=0

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -68,6 +68,9 @@ public class MergeSort {
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
+        if (items.isEmpty()) {
+            return items;
+        }
         Queue<Queue<Item>> q = makeSingleItemQueues(items);
         while (q.size() > 1) {
             Queue<Item> q1 = q.dequeue();
